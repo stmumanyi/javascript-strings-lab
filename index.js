8af6946eff0651b89fb0e66652a14e8d46829c81
Simba Mumanyi <simbamumanyi@gmail.com>
2019-12-07 15:09:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ var greeting = "Hello, everybody!";
 var specialGuest = "Niel de Grasse Tyson";
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 var topic = "space";
-var conversation = "Let's talk about space";
+var conversation = "Let's talk about space.";
